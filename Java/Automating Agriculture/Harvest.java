ff4069902407926ba876294609019ce05f192203
Sadiashraful-KCL <sadiashraful18@gmail.com>
2018-08-03 12:04:44 +0100

    Added a planning project for Java

---
+++ Java/Automating Agriculture/Harvest.java
@@ -0,0 +1,15 @@
+public class Harvest{
+
+  public static void main(String[] args) {
+
+
+
+
+
+
+    //Crop crop = new Crop();
+    //Field field = new Field();
+    //Harvester harvester = new Harvester();
+    //CombineHarvester combineHarvester = new CombineHarvester();
+  }
+}
